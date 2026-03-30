e50bd0f380113da64ee3b39223bcf0288b93fd52
muhammadfaishalnurf <155943817+muhammadfaishalnurf@users.noreply.github.com>
2026-03-30 19:44:52 +0700

    init project react

--- src/pages/Evan.jsx
+++
@@ -1,9 +0,0 @@
-function Evan() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Halaman Evan</h1>
-    </div>
-  );
-}
-
-export default Evan;
--- src/pages/Tiara.jsx
+++
@@ -1,9 +0,0 @@
-function Tiara() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Halaman Tiara</h1>
-    </div>
-  );
-}
-
-export default Tiara;
